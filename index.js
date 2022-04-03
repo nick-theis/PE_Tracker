3d710e5907dc6f4fe322f05e856ff1fb1fa27afe
Nick Theis <theis.nick@gmail.com>
2022-04-02 19:47:34 -0500

    fixed syntax error.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ express()
   .use(express.urlencoded({ extended: true}))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/' async(req, res) => {
+  .get('/', async(req, res) => {
     try {
       const client = await pool.connect();
 
